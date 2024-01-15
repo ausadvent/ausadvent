185f9dd14d2bde59a2976ebd49948441e66d57b9
Juan <sernadominguezj@gmail.com>
2024-01-15 16:23:58 +1100

    The Latest articles component in home is done

--- src/app/components/LatestArticles.tsx
+++ src/app/components/LatestArticles.tsx
@@ -3,6 +3,13 @@
 import React, { useEffect, useState } from 'react'
 
 import { fetchArticles } from '../utils/fetchArticles'
+import Image from 'next/image'
+
+// Assets
+import Left from '../../../assets/blue-left.svg'
+import Right from '../../../assets/blue-right.svg'
+import Eye from '../../../assets/white-eye.svg'
+import WhiteRight from '../../../assets/white-arrow.svg'
 
 export default function LatestArticles() {
     const [articles, setArticles] = useState<any>()
@@ -27,9 +34,71 @@ export default function LatestArticles() {
   
     return (
         <div>
-            <div className='py-[3rem]'>
+            <div className='pt-[3rem]'>
                 <h2 className='cormorant text-[1.5rem] font-bold text-center'>LATEST ARTICLES</h2>
-                <p>{articles[currentArticle]?.fields.articleTitle}</p>
+                {/* Articles box */}
+                <div className='relative mt-[3rem] h-[16rem]'>
+                    {/* Hero */}
+                    <div className='absolute inset-0 box-content z-1 w-full h-[16rem] '>
+                        <Image 
+                            className='inset-0 w-full h-full object-cover opacity-90'
+                            src={articles && articles[currentArticle]?.fields.articleMainImage.fields.file.url}  
+                            title={articles && articles[currentArticle]?.fields.articleMainImage.fields.title}
+                            alt={articles && articles[currentArticle]?.fields.articleMainImage.fields.description}
+                            width={100}
+                            height={100}
+                            unoptimized
+                            loading='eager'
+                        />
+                        {/* to-[#416AF1] */}
+                        <div className="absolute inset-0 bg-gradient-to-b from-transparent to-[#16244B] opacity-70 " aria-hidden="true"></div>
+                    </div>
+                    {/* Text */}
+                    <div className="relative py-[1rem] px-[0.5rem] h-full flex flex-col justify-end gap-[1rem]">
+                        <div className="w-full flex justify-between items-start gap-[0.5rem]">
+                            <Image
+                                src={Left} 
+                                title='Left arrow' 
+                                alt='Left arrow' 
+                                onClick={() => {
+                                    if(currentArticle > 0) {
+                                        setCurrentArticle(currentArticle -1)
+                                    } else {
+                                        setCurrentArticle(recentArticles.length - 1)
+                                    }
+                                }}
+                            />
+                            <div className='flex flex-col gap-[1rem] text-primaryWhite'>
+                                <h3 className='cormorant text-[1.875rem] font-bold leading-[1.875rem]'>{articles && articles[currentArticle]?.fields?.articleTitle && articles[currentArticle]?.fields?.articleTitle.toUpperCase()}</h3>
+                                <div className="flex gap-[0.5rem] items-center">
+                                    <Image src={Eye} title='Eye icon' alt='Eye icon' />
+                                    <p>Read</p>
+                                    <Image src={WhiteRight} title='Right arrow' alt='Right arrow' />
+                                </div>
+                            </div>
+                            <Image 
+                                src={Right} 
+                                title='Right arrow' 
+                                alt='Right arrow' 
+                                onClick={() => {
+                                    if(currentArticle < recentArticles.length - 1) {
+                                        setCurrentArticle(currentArticle + 1)
+                                    } else {
+                                        setCurrentArticle(0)
+                                    }
+                                }}
+                            />
+                        </div>
+                        
+                        {/* Dots */}
+                        <div className='mx-auto  w-full flex justify-center gap-2'>
+                            <button className={` w-[0.3rem] h-[0.3rem] rounded-full bg-[#2563EB] ${currentArticle === 0 && 'bg-white'}`}></button>
+                            <button className={`w-[0.3rem] h-[0.3rem] rounded-full bg-[#2563EB] ${currentArticle === 1 && 'bg-white'}`}></button>
+                            <button className={`w-[0.3rem] h-[0.3rem] rounded-full bg-[#2563EB] ${currentArticle === 2 && 'bg-white'}`}></button>
+                        </div>
+
+                    </div>
+                </div>
             </div>
         </div>
     )

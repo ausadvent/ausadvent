52e57e54e1be24d95ca771eae4c66d44b86e7fa7
Juan <sernadominguezj@gmail.com>
2024-01-17 14:14:21 +1100

    Missing Footer for md devices

--- src/app/components/Independence.tsx
+++ src/app/components/Independence.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function Independence() {
   return (
     <div className='bg-[#1D4ED8]'>
-        <div className='page sm:mx-auto py-[3rem] text-primaryWhite flex flex-col gap-[2rem]'>
-            <h4 className='cormorant text-[2.7rem] sm:text-[3rem] leading-[3rem] font-bold'>DISCOVER YOUR PATHWAY TO INDEPENDENCE</h4>
+        <div className='page sm:mx-auto py-[3rem] md:py-[3.5rem] text-primaryWhite flex flex-col gap-[2rem]'>
+            <h4 className='cormorant text-[2.7rem] sm:text-[3rem] leading-[3rem] font-bold'>DISCOVER YOUR PATH TO INDEPENDENCE</h4>
             <p className='text-[1.25rem]'>Supporting over 5,000 people</p>
             <button className='button py-[0.5rem] rounded-lg text-[1rem] font-semibold'>Contact us today!</button>
         </div>

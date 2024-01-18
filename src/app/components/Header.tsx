38232491758fb50a8a400d058ee17266b59afd01
Juan <sernadominguezj@gmail.com>
2024-01-18 14:05:11 +1100

    Home is done till 2xl devices, inclusive

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -73,7 +73,7 @@ export default function Header() {
             </Link>
             <button
                 id='hamburguer-button'
-                className=' cursor-pointer relative'
+                className=' cursor-pointer relative xl:hidden'
                 onClick={() => {
                     setMobileMenu(!mobileMenu)
                 }}
@@ -93,47 +93,42 @@ export default function Header() {
                     </svg>
                 )}
             </button>
-        </div>
 
-            {/* Mobile menu area */}
-
-            {mobileMenu === true && (
-                <div className={`absolute z-10 origin-top bg-[#2563EB] w-screen ${mobileMenu && 'animate-open-menu'} `} >
-                    
-                    <nav className='min-h-full p-[2rem] sm:px-[7rem] text-primaryWhite' aria-label='mobile'>
-                        <ul className='flex flex-col gap-[1.25rem] text-[1.25rem]'>
-                            <li>Home</li>
-                            <li>About us</li>
-                            <li className='flex items-center gap-[0.2rem]'>
-                                <p>Services</p>
-                                {!servicesDisplay ? (
-                                    <svg 
-                                        onClick={() => {
-                                            setServicesDisplay(true)
-                                        }}
-                                        className="h-6 w-6 text-[#F59E0B]"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"
-                                        >  
-                                        <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="6 9 12 15 18 9" />
-                                    </svg>
-                                ) : (
-                                    <svg 
-                                        onClick={() => {
-                                        setServicesDisplay(false)
-                                        }}
-                                        className="h-6 w-6 text-[#F59E0B]"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  
-                                        <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="6 15 12 9 18 15" />
-                                  </svg>
-                                )}
-                            </li>
-                            {/* Services display for mobile */}
+            {/* Nav area for desktop */}
+            <div className="hidden xl:flex ">
+                <nav className='w-[50rem] ' >
+                    <ul className='flex justify-between items-center text-primaryWhite'>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li className='flex items-center gap-[0.2rem]'>
+                            <p>Services</p>
+                            {!servicesDisplay ? (
+                                <svg 
+                                    onClick={() => {
+                                        setServicesDisplay(true)
+                                        setLocationsDisplay(false)
+                                    }}
+                                    className="h-6 w-6 text-[#F59E0B] xl:hover:cursor-pointer"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"
+                                    >  
+                                    <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="6 9 12 15 18 9" />
+                                </svg>
+                            ) : (
+                                <svg 
+                                    onClick={() => {
+                                    setServicesDisplay(false)
+                                    }}
+                                    className="h-6 w-6 text-[#F59E0B] xl:hover:cursor-pointer"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  
+                                    <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="6 15 12 9 18 15" />
+                                </svg>
+                            )}
+                            {/* Services display for desktop */}
                             {servicesDisplay && (
-                                <div className='pl-[1.5rem] flex flex-col gap-[0.5rem] text-[1rem] text-secondaryWhite  animate-open-services origin-top-left'>
+                                <div className='absolute pl-[0.1rem] mt-[10rem] flex flex-col gap-[0.5rem] text-[1rem] text-secondaryWhite animate-open-services origin-top-left'>
                                     {services.map((service:any) => (
                                         <Link
                                             key={service.id}
                                             href={service.route}
                                             onClick={() => {
-                                                setMobileMenu(!mobileMenu)
                                                 setServicesDisplay(false)
                                             }}
                                         >
@@ -142,36 +137,38 @@ export default function Header() {
                                     ))}
                                 </div>
                             )}
-                            <li className='flex items-center gap-[0.2rem]'>
-                                <p>Locations</p>
-                                {!locationsDisplay ? (
-                                    <svg 
-                                        onClick={() => {
-                                            setLocationsDisplay(true)
-                                        }}
-                                        className="h-6 w-6 text-[#F59E0B]"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"
-                                        >  
-                                        <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="6 9 12 15 18 9" />
-                                    </svg>
-                                ) : (
-                                    <svg 
-                                        onClick={() => {
-                                        setLocationsDisplay(false)
-                                        }}
-                                        className="h-6 w-6 text-[#F59E0B]"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  
-                                        <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="6 15 12 9 18 15" />
-                                    </svg>
-                                )}
-                            </li>
+                        </li>
+                        <li>NDIS</li>
+                        <li className='flex items-center gap-[0.2rem]'>
+                            <p>Locations</p>
+                            {!locationsDisplay ? (
+                                <svg 
+                                    onClick={() => {
+                                        setLocationsDisplay(true)
+                                        setServicesDisplay(false)
+                                    }}
+                                    className="h-6 w-6 text-[#F59E0B] xl:hover:cursor-pointer"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"
+                                    >  
+                                    <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="6 9 12 15 18 9" />
+                                </svg>
+                            ) : (
+                                <svg 
+                                    onClick={() => {
+                                    setLocationsDisplay(false)
+                                    }}
+                                    className="h-6 w-6 text-[#F59E0B] xl:hover:cursor-pointer"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  
+                                    <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="6 15 12 9 18 15" />
+                                </svg>
+                            )}
+                            {/* Services display for desktop */}
                             {locationsDisplay && (
-                                <div className='pl-[1.5rem] flex flex-col gap-[0.5rem] text-[1rem] text-secondaryWhite animate-open-services origin-top-left'>
+                                <div className='absolute pl-[0.1rem] mt-[5.5rem] flex flex-col gap-[0.5rem] text-[1rem] text-secondaryWhite animate-open-services origin-top-left'>
                                     {locations.map((location:any) => (
                                         <Link
                                             key={location.id}
                                             href={location.route}
                                             onClick={() => {
-                                                setMobileMenu(!mobileMenu)
-                                                setServicesDisplay(false)
+                                                setLocationsDisplay(false)
                                             }}
                                         >
                                             <p>{location.title}</p>
@@ -179,23 +176,125 @@ export default function Header() {
                                     ))}
                                 </div>
                             )}
-                            <li>Articles</li>
-                            <li>
-                                <a href="tel:+61439430007" className='flex items-center gap-1'>
-                                    <Image src={Phone} className='h-[1.5rem] w-[1.5rem]' alt='phone ringing icon' />
-                                    <p>+61 0439430007</p>
-                                </a>
-                            </li>
-                            <li>
-                                <button className='button  text-[1rem] py-[0.25rem] rounded'>
-                                    Get in touch
-                                </button>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            )}
-        
+                        </li>
+                        <li>Articles</li>
+                        <li>
+                            <a href="tel:+61439430007" className='flex items-center gap-1'>
+                                <Image src={Phone} className='h-[1.5rem] w-[1.5rem]' alt='phone ringing icon' />
+                                <p>+61 0439430007</p>
+                            </a>
+                        </li>
+                        <li>
+                            <button className='button px-[1rem] text-[1rem] py-[0.25rem] rounded hover:bg-none hover:text-primaryWhite hover:border hover:border-[#FDBA74] transition duration-300 ease-linear '>
+                                Get in touch
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+
+        {/* Mobile menu area */}
+
+        {mobileMenu === true && (
+            <div className={`absolute z-10 origin-top bg-[#2563EB] w-screen ${mobileMenu && 'animate-open-menu'} `} >
+                
+                <nav className='min-h-full p-[2rem] sm:px-[7rem] text-primaryWhite' aria-label='mobile'>
+                    <ul className='flex flex-col gap-[1.25rem] text-[1.25rem]'>
+                        <li>Home</li>
+                        <li>About us</li>
+                        <li className='flex items-center gap-[0.2rem]'>
+                            <p>Services</p>
+                            {!servicesDisplay ? (
+                                <svg 
+                                    onClick={() => {
+                                        setServicesDisplay(true)
+                                    }}
+                                    className="h-6 w-6 text-[#F59E0B]"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"
+                                    >  
+                                    <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="6 9 12 15 18 9" />
+                                </svg>
+                            ) : (
+                                <svg 
+                                    onClick={() => {
+                                    setServicesDisplay(false)
+                                    }}
+                                    className="h-6 w-6 text-[#F59E0B]"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  
+                                    <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="6 15 12 9 18 15" />
+                                </svg>
+                            )}
+                        </li>
+                        {/* Services display for mobile */}
+                        {servicesDisplay && (
+                            <div className='pl-[1.5rem] flex flex-col gap-[0.5rem] text-[1rem] text-secondaryWhite  animate-open-services origin-top-left'>
+                                {services.map((service:any) => (
+                                    <Link
+                                        key={service.id}
+                                        href={service.route}
+                                        onClick={() => {
+                                            setMobileMenu(!mobileMenu)
+                                            setServicesDisplay(false)
+                                        }}
+                                    >
+                                        <p>{service.title}</p>
+                                    </Link>
+                                ))}
+                            </div>
+                        )}
+                        <li className='flex items-center gap-[0.2rem]'>
+                            <p>Locations</p>
+                            {!locationsDisplay ? (
+                                <svg 
+                                    onClick={() => {
+                                        setLocationsDisplay(true)
+                                    }}
+                                    className="h-6 w-6 text-[#F59E0B]"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round"
+                                    >  
+                                    <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="6 9 12 15 18 9" />
+                                </svg>
+                            ) : (
+                                <svg 
+                                    onClick={() => {
+                                    setLocationsDisplay(false)
+                                    }}
+                                    className="h-6 w-6 text-[#F59E0B]"  width="24" height="24" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  
+                                    <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="6 15 12 9 18 15" />
+                                </svg>
+                            )}
+                        </li>
+                        {locationsDisplay && (
+                            <div className='pl-[1.5rem] flex flex-col gap-[0.5rem] text-[1rem] text-secondaryWhite animate-open-services origin-top-left'>
+                                {locations.map((location:any) => (
+                                    <Link
+                                        key={location.id}
+                                        href={location.route}
+                                        onClick={() => {
+                                            setMobileMenu(!mobileMenu)
+                                            setServicesDisplay(false)
+                                        }}
+                                    >
+                                        <p>{location.title}</p>
+                                    </Link>
+                                ))}
+                            </div>
+                        )}
+                        <li>Articles</li>
+                        <li>
+                            <a href="tel:+61439430007" className='flex items-center gap-1'>
+                                <Image src={Phone} className='h-[1.5rem] w-[1.5rem]' alt='phone ringing icon' />
+                                <p>+61 0439430007</p>
+                            </a>
+                        </li>
+                        <li>
+                            <button className='button  text-[1rem] py-[0.25rem] rounded'>
+                                Get in touch
+                            </button>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        )}
+    
     </div>
   )
 }

f332745ca2974eb02dd4ddb6cc150c4a5a977c30
Juan <sernadominguezj@gmail.com>
2024-01-24 16:53:06 +1100

    Working on Description component of NDIS page

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -19,7 +19,6 @@ export default function Header() {
         }
         fetchingData()
     }, [])
-    console.log(servicesFetched)
     
     // const services = [
     //     {
---
+++ src/app/ndis/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Intro from './Intro'
+import Description from './Description'
+
+export default function Ndis() {
+  return (
+    <main>
+        <Intro />
+        <Description />
+    </main>
+  )
+}

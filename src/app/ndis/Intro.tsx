b36f64b28bfe531c7470c99c5203c9ae0398e838
Juan <sernadominguezj@gmail.com>
2024-01-27 14:13:03 +1100

    NDIS component done for all devices. Also, functional changes in Header

--- src/app/ndis/Intro.tsx
+++ src/app/ndis/Intro.tsx
@@ -19,12 +19,12 @@ export default function Intro() {
 
             {/* Content */}
             <div className=' relative page sm:mx-auto pt-[5rem] sm:pt-[8rem] lg:pt-[12rem] xl:pt-[18rem] pb-[5rem] sm:h-[42rem] md:h-[45rem] xl:h-[59rem] text-primaryWhite flex flex-col gap-[2.5rem] '>
-                <div className='flex flex-col lg:flex-row gap-[2rem] text-center lg:text-left'>
-                    <h1 className='xl:w-1/2 text-[1.5rem] md:text-[1.875rem] lg:text-[2rem] leading-[1.5rem] md:leading-[1.75rem] lg:leading-[2rem] font-black '>WE&apos;RE <span className='text-[#F59E0B]'>YOUR TRUSTED PROVIDERS</span>, EMPOWERING YOU TO NAVIGATE EVERY STEP OF YOUR NDIS JOURNEY WITH CONFIDENCE.</h1>
+                <div className='flex flex-col lg:flex-row gap-[2rem] 3xl:gap-[4rem] text-center lg:text-left'>
+                    <h1 className='xl:w-1/2 text-[1.5rem] md:text-[1.875rem] lg:text-[2rem] 2xl:text-[3rem] leading-[1.5rem] md:leading-[1.75rem] lg:leading-[2rem] 2xl:leading-[2.875rem] font-black '>WE&apos;RE <span className='text-[#F59E0B]'>YOUR TRUSTED PROVIDERS</span>, EMPOWERING YOU TO NAVIGATE EVERY STEP OF YOUR NDIS JOURNEY WITH CONFIDENCE.</h1>
                     <div className='xl:w-1/2 flex flex-col gap-[2rem]'>
-                        <p className='sm:w-[20rem] xl:w-full sm:mx-auto cormorant text-[1.25rem] lg:text-[1.5rem] leading-[1.375rem] lg:leading-[1.625rem] font-bold'>Embarking on your National Disability Insurance Scheme (NDIS) journey can be both exciting and overwhelming.</p>
-                        <p className='sm:w-[20rem]  xl:w-fullsm:mx-auto lg:text-[1.125rem]'>We help you to make the most of your unique plan.</p>
-                        <button className='py-[0.5rem] border-[2px] border-[#F59E0B] rounded-[0.5rem] w-[18.75rem] xl:w-full mx-auto'>Contact us</button>
+                        <p className='sm:w-[20rem] xl:w-full sm:mx-auto cormorant text-[1.25rem] lg:text-[1.5rem] 2xl:text-[1.857rem] leading-[1.375rem] lg:leading-[1.625rem] 2xl:leading-[2.0625rem] font-bold'>Embarking on your National Disability Insurance Scheme (NDIS) journey can be both exciting and overwhelming.</p>
+                        <p className='sm:w-[20rem] xl:w-full sm:mx-auto lg:text-[1.125rem] 2xl:text-[1.25rem] 3xl:text-[1.5rem]'>We help you to make the most of your unique plan.</p>
+                        <button className='py-[0.5rem] border-[2px] border-[#F59E0B] rounded-[0.5rem] w-[18.75rem] xl:w-full mx-auto 2xl:text-[1.25rem] 3xl:text-[1.5rem]'>Contact us</button>
                     </div>
                 </div>
                 

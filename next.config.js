9626b2b72236843c60062d10ff2bf09c4f2e3134
Juan <sernadominguezj@gmail.com>
2024-03-06 09:45:52 +1100

    Adding redirect rules to /contact requests

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['images.ctfassets.net', 'images.ctfassets.net'],
+  images: {
+    domains: ['images.ctfassets.net', 'images.ctfassets.net'],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/contact',
+        destination: '/locations',
+        permanent: true,
       },
+    ]
+  }
 }
 
 module.exports = nextConfig

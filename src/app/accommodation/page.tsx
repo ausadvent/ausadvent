60a094d17a98e4fd890b9df9b77ed669ce89a46d
Juan <sernadominguezj@gmail.com>
2024-07-21 12:14:12 +1000

    Building Accommodation

--- src/app/accommodation/page.tsx
+++ src/app/accommodation/page.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import Intro from './Intro'
+import Features from './Features'
+import NotFound from '../not-found'
 
 export default function Accommodation() {
   return (
-    <div>Accommodation</div>
+    // <div className='sm:mx-auto bg-gray-200 pb-[4rem]'>
+    //     <Intro />
+    //     <Features />
+    // </div>
+    <NotFound />
   )
 }
